57ebf564c2ce0b1430c5bb896d28cd7810ecef9b
Jkutkut <jorgeregonzalez@gmail.com>
2020-02-27 23:44:15 +0100

    matrix creation done

--- RubikCube/functions/matrix.js
+++ RubikCube/functions/matrix.js
@@ -1,18 +1,31 @@
-function matrixIdentity(range){ //2D square matrix 
-  m = emptyMatrix(range);
-  for(let i = 0; i < range; i++){
-    for(let j = 1; j < range - 1; j++){
-      m[i][j] = (i == j)? 1 : 0;
+/* MATRIX CREATION */
+makeMatrix = { //2D square matrix
+  matrixIdentity: function(range){
+    m = emptyMatrix(range);
+    for(let i = 0; i < range; i++){
+      for(let j = 0; j < range; j++){
+        m[i][j] = (i == j)? 1 : 0;
+      }
     }
+    return m;
+  },
+  
+  zeroMatrix: function(range){
+    m = emptyMatrix(range);
+    for(let i = 0; i < range; i++){
+      for(let j = 0; j < range; j++){
+        m[i][j] = 0;
+      }
+    }
+    return m;
+  },
+  
+  emptyMatrix: function(range){
+    m = [];
+    for(let i = 0; i < range; i++){
+      m.push(new Array(range));
+    }
+    return m;
   }
-  console.log(range);
-  return m;
-}
-  function emptyMatrix(range){ //2D square matrix 
-  m = [];
-  for(let i = 0; i < range; i++){
-    m.push(new Array(range));
-  }
-  printArray(m);
-  return m;
+
 }
