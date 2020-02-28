49cd1f73a5936808b296a7812d3f4febe010ee2e
Jkutkut <jorgeregonzalez@gmail.com>
2020-02-28 10:55:40 +0100

    added the funcionality to choose the delimiter when printing

--- RubikCube/functions/matrixRepresentation.js
+++ RubikCube/functions/matrixRepresentation.js
@@ -1,16 +1,20 @@
-function printMatrix_nD(m){
-    console.log(matrixToString(m));
+function printMatrix_nD(m, delimeter){
+    console.log(matrixToString(m, delimeter));
 }
-function matrixToString(m){
+function matrixToString(m, delimeter){
+    console.log(delimeter);
+    let d;
     let str = "[";
     for(let i = 0; i < m.length; i++){
         if(Array.isArray(m[i])){
-            str += matrixToString(m[i]);
+            str += matrixToString(m[i], delimeter);
+            d = (delimeter)? delimeter : "\n";
         }
         else{
             str += m[i];
+            d = ",";
         }
-        str += ((i + 1 < m.length) ? ", " : "");
+        str += ((i + 1 < m.length) ?  d + " " : "");
     }
     str += "]";
     return str;
