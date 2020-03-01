f37100ab8e3ec3bc9a086086be638ceee87a7362
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-01 22:59:14 +0100

    .

--- RubikCube/RubikPiece.js
+++ RubikCube/RubikPiece.js
@@ -6,9 +6,8 @@ class RubikPiece{
     - Keep track of sticker's orientation
   */
   constructor(w, c){
-    this.rMatrix = matrix.make.identity(4);
+    this.matrix = matrix.make.identity(4);
     this.posM;//the actual pos (translation matrix)
-
 
     this.width = (w)? w : cubeW;
     this.color = (c)? c : COLORS[COLORS.length - 1];
@@ -18,14 +17,9 @@ class RubikPiece{
   show(){
     fill(this.color);
     push();
-    strokeWeight(4)
-    // resetMatrix();
-    let m = matrix.o.mult(this.posM, this.rMatrix);
-
-    // applyMatrix(...matrix.p.applyRotation(this.rMatrix));
-    applyMatrix(...matrix.p.applyRotation(m));
-    box(this.width);
-    // translate(this.pos);
+    strokeWeight(4);
+    applyMatrix(...matrix.p.applyRotation(this.matrix));
+    box(this.width,50,25);
     
     for(let i = 0; i < this.stickers.length; i++){
       this.stickers[i].show();
@@ -33,20 +27,24 @@ class RubikPiece{
     pop();
   }
 
-  rotate(axis, angle){
-    this.applyM(matrix.make.rotation4D(axis, angle));
+  rotatePiece(axis, angle){
+    this.applyM(matrix.make.rotation(axis, angle));
+  }
+  rotateOrigin(axis, angle){
+    this.applyM(matrix.make.rotationOrigin(axis, angle));
   }
   applyM(rM){ //rM = rotation matrix
     printMatrix_nD(rM);
-    printMatrix_nD(this.rMatrix);
-    this.rMatrix = matrix.o.mult(rM,this.rMatrix);
+    printMatrix_nD(this.matrix);
+    this.matrix = matrix.o.mult(rM,this.matrix);
   }
   move(posi){
+    this.applyM(matrix.make.translation(posi));
     //moves the piece to a new pos
-    this.pos = posi.copy();
+    // this.pos = posi.copy();
   }
   setPos(posi){
-    this.posM = matrix.make.translation(posi);
+    this.applyM(matrix.make.translation(posi));
   }
 }
 
---
+++ RubikCube/ThingsToDo.md
@@ -0,0 +1,8 @@
+# Things to do:
+
+## JS-Tools:
+
+### Matrix:
+
+-Seems like the rotation 3D is useless.
+-Seems like the rotation Origin = rotation normal.
