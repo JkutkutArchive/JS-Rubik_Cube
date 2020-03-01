231ed5d820c94ef217aee4f0a3e1031f241b68a1
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-01 19:21:46 +0100

    matrix implementation seems to work

--- RubikCube/RubikPiece.js
+++ RubikCube/RubikPiece.js
@@ -5,21 +5,37 @@ class RubikPiece{
     - Use matrices
     - Keep track of sticker's orientation
   */
-  constructor(pos){
+  constructor(pos, width, c){
     this.pos = pos;//the actual pos
-    // this.moves = [];//[["x", 0.5]] -> rotateX(Math.PI / 2);
-  }
-  show(){
-    /*
-    size = cubeW
-    color = last color in 'colors' array
-    */
-    
+    this.width = (width)? width : cubeW;
+    // this.color = (c)? c : COLORS[COLORS.length - 1];
+    this.rMatrix = matrix.make.identity(4);
+    this.stickers = [];
   }
 
-  rotate(rM){
-    //rM = rotation matrix
-    console.log("holaaa");
+  show(){
+    // fill(this.color);
+    push();
+    strokeWeight(4)
+    // resetMatrix();
+    applyMatrix(...this.applyRotation(this.rMatrix));
+    box(this.width);
+    // translate(this.pos);
+    
+    for(let i = 0; i < this.stickers.length; i++){
+      this.stickers[i].show();
+    }
+    pop();
+  }
+  applyRotation(m){
+    return [m[0][0],m[0][1],m[0][2],0,m[1][0],m[1][1],m[1][2],0,m[2][0],m[2][1],m[2][2],0,0,0,0,1];
+  }
+
+  rotate(axis, angle){
+    this.applyM(matrix.make.rotate(axis, angle));
+  }
+  applyM(rM){ //rM = rotation matrix
+    this.rMatrix = matrix.o.mult(this.rMatrix, rM);
   }
   move(posi){
     //moves the piece to a new pos
@@ -30,13 +46,12 @@ class RubikPiece{
 class Edge extends RubikPiece{
   constructor(pos){
     super(pos);
-    this.stickers = [];
   }
 }
 
 class Corner extends RubikPiece{
   constructor(pos){
     super(pos);
-    this.stickers = [];
+    
   }
 }
