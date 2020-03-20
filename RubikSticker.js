121d31e5de6596f4e5328db2bf4dbe557cda9a3d
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-20 13:44:48 +0100

    Added JSDOC

--- RubikSticker.js
+++ RubikSticker.js
@@ -1,22 +1,40 @@
+/**
+ * @class
+ * @classdesc This class enable us to generate Rubik's cube stickers.
+ */
 class RubikSticker{
+    /**
+     * Inicialites the sticker
+     * @param {number} rX - Number of rotations of 90 degres around the X axis.
+     * @param {number} rY - Number of rotations of 90 degres around the Y axis.
+     * @param {P5Color} [c=COLORSDIC.NULL] - Color of the sticker (P5Color)
+     * @param {number} [w=cubeW]  - The size of the piece that contains the sticker.
+     */
     constructor(rX, rY, c, w){
         this.rX = rX * Math.PI / 2;
         this.rY = rY * Math.PI / 2;
-
         this.width = (w)? w : cubeW;
-        this.c = (c)? c : COLORS[3];
+        this.c = (c)? c : COLORSDIC.NULL;
     }
+    /**
+     * Prints on screen the sticker.
+     */
     show(){
         push();
-        noStroke();
-        fill(this.c);
-        rotateX(this.rX);
-        rotateY(this.rY);
-        translate(0,0, this.width * 0.52);
+        noStroke(); //Without borders
+        fill(this.c); //Fill with sticker's color
+        //Place at correct position
+        rotateX(this.rX); //Rotate on the X axis
+        rotateY(this.rY); //Rotate on the Y axis
+        translate(0,0, this.width * 0.52); //Translate to the face of the cube (width * (0.5 + offset to see the sticker)).
         
-        plane(this.width * 0.88);
+        plane(this.width * 0.88); //Draw the sticker a bit smaller than the piece
         pop();
     }
+    /**
+     * Changes the color of the sticker
+     * @param {P5Color} color (P5Color)
+     */
     setColor(color){
         this.c = color;
     }
