1d7183939e20f0123588bad685b9f55d3e2b5869
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-27 19:28:10 +0100

    added option to change the width and the percentage width

--- RubikSticker.js
+++ RubikSticker.js
@@ -14,6 +14,7 @@ class RubikSticker{
         this.rX = rX * Math.PI / 2;
         this.rY = rY * Math.PI / 2;
         this.width = (w)? w : RubikCube.cubeW;
+        this.wPercent = 0.88;
         this.c = (c)? c : COLORSDIC.NULL;
     }
     /**
@@ -28,7 +29,7 @@ class RubikSticker{
         rotateY(this.rY); //Rotate on the Y axis
         translate(0,0, this.width * 0.52); //Translate to the face of the cube (width * (0.5 + offset to see the sticker)).
         
-        plane(this.width * 0.88); //Draw the sticker a bit smaller than the piece
+        plane(this.width * this.wPercent); //Draw the sticker a bit smaller than the piece
         pop();
     }
     /**
@@ -37,5 +38,35 @@ class RubikSticker{
      */
     setColor(color){
         this.c = color;
+    }
+    /**
+     * Changes the with of the sticker
+     * @param {number} w - The new width
+     */
+    setWidth(w){
+        try{
+            if(typeof(w) != "number"){
+                throw "it is not a number";
+            }
+            this.w = w;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    /**
+     * Changes the percentage that multiplies the width in order to generate the sticker.
+     * @param {number} wP - The new percentage to generate the sticker
+     */
+    setWPercent(wP){
+        try{
+            if(typeof(w) != "number"){
+                throw "it is not a number";
+            }
+            this.wPercent = wP;
+        }
+        catch(error){
+            console.log(error);
+        }
     }
 }
