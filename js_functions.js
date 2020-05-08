13739b8c67a19b563050dced2de8f193a8202057
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-08 23:45:44 +0200

    All main screen will be made using p5. See changeDraw function on mainCanvas 2

---
+++ js_functions.js
@@ -0,0 +1,18 @@
+
+// launchFullScreen(document.documentElement); // the whole page
+
+
+
+
+
+
+
+function launchFullScreen(element) {
+    if(element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+  }
